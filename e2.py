921c9f8251dcefe229b2845c819e4cce0f571a46
Emma Paris <kimmynav@uw.edu>
2024-03-28 11:30:33 -0700

    editing and adding files

--- e2.py
+++ e2.py
@@ -6,3 +6,4 @@ x2 = np.random.randint(10, size=(3, 4))  # Two-dimensional array
 x3 = np.random.randint(10, size=(3, 4, 5))  # Three-dimensional array
 
 print(x1)
+#Each array has attributes ndim (the number of dimensions), shape (the size of each dimension), and size (the total size of the array):
